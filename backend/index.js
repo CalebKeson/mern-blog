065c35e677032045b64468987519cd9b54a30dd2
Caleb <lebokeson@gmail.com>
2025-05-08 20:19:40 +0300

    connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
-import express from 'express';
-import cors from 'cors'; 
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
 // create express app
 const app = express();
 
+// configure the database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Successfully connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
 // cors
-app.use(cors())
+app.use(cors());
 
 // listen on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}!`);
+  console.log(`Server is running on port ${PORT}!`);
 });
