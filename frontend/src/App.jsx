3369ac9e89d2f3ad70da211b3c4f9927f1c7d9cf
Caleb <lebokeson@gmail.com>
2025-05-08 10:31:29 +0300

    install react js and tailwind css and create the first template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h2 className="text-green-700 text-center text-3xl">
+      Welcome to MERN Blog!
+    </h2>
+  );
+};
+
+export default App;
